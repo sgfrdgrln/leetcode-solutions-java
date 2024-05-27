12ba9b97ec72999544cf832beb8e43b027fe8d86
sgfrdgrln <gazel0123@gmail.com>
2024-05-27 19:20:27 +0800

    palindrome number solution

--- PalindromeNumSolution.java
+++ PalindromeNumSolution.java
@@ -11,6 +11,7 @@ public class PalindromeNumSolution {
     }
     public static boolean isPalindrome(int x) {
 
+        // Early return for negative numbers and numbers that end in 0 but are not 0
         if(x < 0 || (x != 0 && x % 10 == 0)) {
             return false;
         }
@@ -19,9 +20,11 @@ public class PalindromeNumSolution {
 
         while (x > reversedHalf) {
             
+             // Reverse half of the number
             reversedHalf = reversedHalf * 10 + x % 10;
             x /= 10;
         }
+        // Check if the number is a palindrome
         return x == reversedHalf || x == reversedHalf / 10;
     }
 }
