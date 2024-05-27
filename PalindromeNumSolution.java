f2cfc35af8e311bb4076b95a7404664fcbbfe91d
sgfrdgrln <gazel0123@gmail.com>
2024-05-27 19:15:54 +0800

    palindrome number solution

---
+++ PalindromeNumSolution.java
@@ -0,0 +1,27 @@
+public class PalindromeNumSolution {
+
+    // Palindrome Number
+    
+    public static void main(String[] args) {
+        
+
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(121));
+
+    }
+    public static boolean isPalindrome(int x) {
+
+        if(x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+}
